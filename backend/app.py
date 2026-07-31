daa3d404a8649615a5a17bed6c034a0c035a71a6
adhishintha <51943361+adhishintha@users.noreply.github.com>
2026-07-31 15:04:56 +0530

    Fix main block to run server on all interfaces

--- backend/app.py
+++ backend/app.py
@@ -192,5 +192,5 @@ def delete_leave(id):
 
 
 # -------------------- Run Server --------------------
-if __name__ == "__main__":
-    app.run(debug=True)
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000, debug=True)
