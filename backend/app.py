0bc08c010209a4fdda10c8a2c306d73d40dd81ad
adhishintha <51943361+adhishintha@users.noreply.github.com>
2026-07-31 15:33:07 +0530

    Fix main check in app.py

--- backend/app.py
+++ backend/app.py
@@ -192,5 +192,5 @@ def delete_leave(id):
 
 
 # -------------------- Run Server --------------------
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
