9946d7c31c775bd942f0f98a7831418a040a4798
adhishintha <51943361+adhishintha@users.noreply.github.com>
2026-07-31 11:47:49 +0530

    Update database password in config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "Daneshsdurgada@2002@",
+    "password": "Adhi3232$",
     "database": "employee_leave_management"
     }
